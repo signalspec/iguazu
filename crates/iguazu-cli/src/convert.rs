f5d37e6abf9e952a23fdb69a5216d718a15fa707
Kevin Mehall <km@kevinmehall.net>
2025-08-25 19:04:05 -0600

    cli: convert: Add --build-summary option

--- crates/iguazu-cli/src/convert.rs
+++ crates/iguazu-cli/src/convert.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use clap::Args;
-use iguazu::{cli::{ExportOpts, ImportOpts}, export::EXPORTERS, import::IMPORTERS, io::FsFile};
+use iguazu::{cli::{ExportOpts, ImportOpts}, export::EXPORTERS, import::IMPORTERS, storage::MemoryStorage};
 use futures_lite::future::{self, block_on};
 
 #[derive(Args)]
@@ -10,6 +10,9 @@ pub struct Cli {
     #[clap(flatten)]
     import: ImportOpts,
 
+    #[clap(long, help = "Build default summary of all entities")]
+    build_summary: bool,
+
     #[clap(flatten)]
     export: ExportOpts,
 }
@@ -17,7 +20,12 @@ pub struct Cli {
 pub fn main(args: &Cli) -> Result<(), String> {
     let executor = Arc::new(async_executor::Executor::new());
     block_on(executor.run(async {
-        let (entity, import_completion) = args.import.import(IMPORTERS, executor.clone()).await?;
+        let (mut entity, import_completion) = args.import.import(IMPORTERS, executor.clone()).await?;
+
+        if args.build_summary {
+            entity.build_summaries(&executor, &MemoryStorage);
+        }
+
         let export_completion = args.export.export(EXPORTERS, executor.clone(), entity);
 
         future::try_zip(
