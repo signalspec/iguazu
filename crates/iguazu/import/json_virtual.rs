d9631fc8946626688e86b672d354b1c9c2892be1
Kevin Mehall <km@kevinmehall.net>
2025-08-25 19:04:05 -0600

    Entity::try_map_data_async

--- crates/iguazu/import/json_virtual.rs
+++ crates/iguazu/import/json_virtual.rs
@@ -1,11 +1,8 @@
 use std::{pin::Pin, sync::Arc};
 
-use async_executor::{ Executor, Task };
-use ecow::EcoString;
-use futures_lite::{stream, StreamExt};
-use indexmap::IndexMap;
+use async_executor:: Executor;
 
-use crate::{io::{ReadableFile}, schema::{json_virtual::StreamRef, Entity, EntitySchema, EntityStream, Summary}, storage::{ FlatFileOpts, FlatFileStream }, stream::ArcStream};
+use crate::{io::{ReadableFile}, schema::{json_virtual::StreamRef, Entity, EntitySchema, EntityStream}, storage::{ FlatFileOpts, FlatFileStream }, stream::ArcStream};
 
 use super::{ImportError, Importer};
 
@@ -39,7 +36,7 @@ impl Importer for VirtualImporter {
             let file = self.file;
             let schema = self.schema.unwrap();
 
-            let entity = load(file, executor, schema).await?;
+            let entity = schema.try_map_data_async(move |s| create_stream(file.clone(), executor.clone(), s)).await?;
             Ok((entity, Box::pin(async move {Ok(())}) as Pin<Box<_>>))
         })
     }
@@ -49,89 +46,14 @@ pub fn importer(file: Arc<dyn ReadableFile>) -> Box::<dyn Importer> {
     Box::new(VirtualImporter { file, schema: None })
 }
 
-pub async fn load(file: Arc<dyn ReadableFile>, io_executor: Arc<Executor<'static>>, schema: Entity<StreamRef>) -> Result<EntityStream, ImportError> {
-    let executor = Arc::new(Executor::new());
-
-    async fn create_stream(src_file: Arc<dyn ReadableFile>, io_executor: Arc<Executor<'static>>, stream: StreamRef) -> Result<ArcStream, ImportError> {
-        match stream {
-            StreamRef::FlatFile { ref file_name, element_type, offset } => {
-                let file = src_file.relative(file_name).await?;
-                let mut opts = FlatFileOpts::default();
-                opts.element_type = element_type;
-                opts.offset = offset;
-                Ok(Arc::new(FlatFileStream::new(file, io_executor, opts).await?))
-            }
+async fn create_stream(src_file: Arc<dyn ReadableFile>, io_executor: Arc<Executor<'static>>, stream: StreamRef) -> Result<ArcStream, ImportError> {
+    match stream {
+        StreamRef::FlatFile { ref file_name, element_type, offset } => {
+            let file = src_file.relative(file_name).await?;
+            let mut opts = FlatFileOpts::default();
+            opts.element_type = element_type;
+            opts.offset = offset;
+            Ok(Arc::new(FlatFileStream::new(file, io_executor, opts).await?))
         }
     }
-
-    async fn create_children(ex: Arc<Executor<'static>>, io_executor: Arc<Executor<'static>>, src_file: Arc<dyn ReadableFile>, children: IndexMap<EcoString, Entity<StreamRef>>) -> Result<IndexMap<EcoString, EntityStream>, ImportError> {
-        let child_tasks: Vec<(EcoString, Task<_>)> = children.into_iter().map(|(k, v)| {
-            (k, ex.spawn(create(ex.clone(), io_executor.clone(), src_file.clone(), v)))
-        }).collect();
-
-        stream::iter(child_tasks)
-            .then(|(k, f_v)| async move { Ok::<_, ImportError>((k, f_v.await?)) })
-            .try_collect().await
-    }
-
-    async fn create_summary(ex: Arc<Executor<'static>>, io_executor: Arc<Executor<'static>>, src_file: Arc<dyn ReadableFile>, summary: Summary<StreamRef>) -> Result<Summary<ArcStream>, ImportError> {
-        let Summary { base_level, levels } = summary;
-        let child_tasks: Vec<Task<_>> = levels.into_iter().map(|s| {
-            ex.spawn(create_stream(src_file.clone(), io_executor.clone(), s))
-        }).collect();
-
-        let levels = stream::iter(child_tasks).then(|f| f).try_collect().await?;
-        Ok(Summary { base_level, levels })
-    }
-
-
-    async fn create_summaries(ex: Arc<Executor<'static>>, io_executor: Arc<Executor<'static>>, src_file: Arc<dyn ReadableFile>, summaries: IndexMap<EcoString, Summary<StreamRef>>) -> Result<IndexMap<EcoString, Summary<ArcStream>>, ImportError> {
-        let child_tasks: Vec<(EcoString, Task<_>)> = summaries.into_iter().map(|(k, v)| {
-            (k, ex.spawn(create_summary(ex.clone(), io_executor.clone(), src_file.clone(), v)))
-        }).collect();
-
-        stream::iter(child_tasks)
-            .then(|(k, f_v)| async move { Ok::<_, ImportError>((k, f_v.await?)) })
-            .try_collect().await
-    }
-
-    fn create(ex: Arc<Executor<'static>>, io_executor: Arc<Executor<'static>>, src_file: Arc<dyn ReadableFile>, schema: Entity<StreamRef>) -> impl Future<Output = Result<EntityStream, ImportError>> + Send {
-        async move {
-            match schema {
-                Entity::Group { children, attributes } => {
-                    let children = create_children(ex, io_executor, src_file, children).await?;
-                    Ok(Entity::Group { children, attributes: attributes.clone() })
-                }
-                Entity::Record { children, attributes } => {
-                    let children = create_children(ex, io_executor, src_file, children).await?;
-                    Ok(Entity::Record { children, attributes: attributes.clone() })
-                }
-                Entity::Data { data, ref field, summaries } => {
-                    let data = create_stream(src_file.clone(), io_executor.clone(), data).await?;
-                    let summaries = create_summaries(ex, io_executor, src_file, summaries).await?;
-                    Ok(Entity::Data { field: field.clone(), data, summaries })
-                }
-                Entity::Union { data, variants, attributes } => {
-                    let data = create_stream(src_file.clone(), io_executor.clone(), data).await?;
-                    let variants = create_children(ex, io_executor, src_file, variants).await?;
-                    Ok(Entity::Union { data, variants, attributes: attributes.clone() })
-                }
-                Entity::FixedArray { elements, child, attributes } => {
-                    let child = Box::new(ex.spawn(create(ex.clone(), io_executor, src_file, *child)).await?);
-                    Ok(Entity::FixedArray { elements, child, attributes: attributes.clone() })
-                }
-                Entity::Tuple { fields, child, attributes } => {
-                    let child = Box::new(ex.spawn(create(ex.clone(), io_executor, src_file, *child)).await?);
-                    Ok(Entity::Tuple { fields, child, attributes: attributes.clone() })
-                }
-                Entity::VariableArray { data, child, attributes } => {
-                    let data = create_stream(src_file.clone(), io_executor.clone(), data).await?;
-                    let child = Box::new(ex.spawn(create(ex.clone(), io_executor, src_file, *child)).await?);
-                    Ok(Entity::VariableArray { data, child, attributes: attributes.clone() })
-                }
-            }
-        }
-    }
-
-    executor.run(create(executor.clone(), io_executor, file, schema)).await
 }
