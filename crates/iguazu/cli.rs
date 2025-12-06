signalspec/iguazu

commit d1465ce1e10d119097e01d26b94597e74f6cac70

viewer: Welcome screen and file picker

crates/iguazu/cli.rs
@@ -6,8 +6,10 @@ use clap::Args;
 use crate::{export::ExportFormat, import::{ImportError, ImportFormats, Importer}, io::{FsFile, FsWritableFile, ReadableFile}, schema::{EntitySchema, EntityStream}};
 
 #[derive(Args, Clone, Debug)]
+#[group(requires = "filename")] // Allow `ImportOpts` to be optional (https://github.com/clap-rs/clap/issues/5092#issuecomment-1703980717)
 pub struct ImportOpts {
     /// Input filename
+    #[arg(required = false)]
     pub filename: PathBuf,
 
     /// Input format (if not specified, inferred from filename)
@@ -89,8 +91,7 @@ impl ImportOpts {
     /// Returns the imported entity, as well as a future that completes when the import is fully done.
     pub async fn import(&self, importers: ImportFormats<'_>, executor: Arc<Executor<'static>>) -> Result<(EntityStream, Pin<Box<dyn Future<Output = Result<(), ImportError>> + Send>>), String> {
         let importer = self.importer(importers).await?;
-        let schema = self.schema().await?;
-        let (mut entity, completion) = importer.import(schema, executor).await
+        let (mut entity, completion) = importer.import(None, executor).await
             .map_err(|e| format!("Failed to import {}: {}", self.filename.display(), e))?;
 
         if let Some(ref entity_path) = self.entity {

crates/iguazu-egui/Cargo.toml
@@ -19,6 +19,7 @@ egui.workspace = true
 log.workspace = true
 clap.workspace = true
 ecow.workspace = true
+async-executor.workspace = true
 serde = "1.0.160"
 egui_table = "0.5.0"
 itertools = "0.13.0"

crates/iguazu-egui/src/lib.rs
@@ -6,8 +6,9 @@ mod egui_util;
 pub mod timeline;
 pub mod table;
 
-use std::sync::Arc;
+use std::{future::Future, pin::Pin, sync::Arc, task::{Poll, Waker}};
 
+use async_executor::Executor;
 use iguazu::view::ViewManager;
 pub use time::{ Time, TimeRange };
 pub use timeline::TimelineView;
@@ -27,13 +28,20 @@ impl std::task::Wake for RepaintWaker {
 }
 
 pub struct ViewerContext {
+    executor: Arc<Executor<'static>>,
+
     view_manager: ViewManager,
+
+    /// Waker that requests egui repaint on wake.
+    waker: Waker,
 }
 impl ViewerContext {
-    pub fn new(egui_ctx: &egui::Context) -> Self {
-        let waker = Arc::new(RepaintWaker { context: egui_ctx.clone() }).into();
+    pub fn new(executor: Arc<Executor<'static>>, egui_ctx: &egui::Context) -> Self {
+        let waker: Waker = Arc::new(RepaintWaker { context: egui_ctx.clone() }).into();
         Self {
-            view_manager: ViewManager::new(waker),
+            executor,
+            view_manager: ViewManager::new(waker.clone()),
+            waker,
         }
     }
 
@@ -43,5 +51,30 @@ impl ViewerContext {
 
     pub fn end(&mut self) {
         self.view_manager.end();
+    }
+
+    pub fn executor(&self) -> &Arc<Executor<'static>> {
+        &self.executor
+    }
+
+    pub fn spawn<F>(&self, fut: F) -> async_executor::Task<F::Output>
+    where
+        F: Future + Send + 'static,
+        F::Output: Send + 'static,
+    {
+        self.executor.spawn(fut)
+    }
+
+    pub fn waker(&self) -> &Waker {
+        &self.waker
+    }
+
+    pub fn poll<T>(&self, f: Pin<&mut impl Future<Output = T>>) -> Poll<T> {
+        let mut cx = std::task::Context::from_waker(&self.waker);
+        f.poll(&mut cx)
+    }
+
+    pub fn poll_unpin<T>(&self, f: &mut (impl Future<Output = T> + Unpin)) -> Poll<T> {
+        self.poll(Pin::new(f))
     }
 }

crates/iguazu-viewer/Cargo.toml
@@ -9,7 +9,7 @@ license.workspace = true
 repository.workspace = true
 
 [dependencies]
-iguazu = { workspace = true, features = ["clap"] }
+iguazu = { workspace = true, features = ["clap", "web"] }
 iguazu-egui.workspace = true
 egui.workspace = true
 eframe = { version = "0.33.0", default-features = false, features = ["accesskit", "default_fonts", "wgpu", "wayland"] }
@@ -17,6 +17,7 @@ futures-lite = "2.6.0"
 clap = { workspace = true, features = ["derive"] }
 async-executor.workspace = true
 wgpu = { version = "27", features = ["webgl"] }
+rfd = { git = "https://github.com/kevinmehall/rfd.git", rev = "afab21fefab230667f8106ac2c189f8c670ac054", features = ["file-handle-inner"] }
 
 [target.'cfg(not(target_arch = "wasm32"))'.dependencies]
 tracing-subscriber = "0.3.16"
@@ -25,3 +26,4 @@ tracing-subscriber = "0.3.16"
 wasm-bindgen-futures = "0.4.50"
 web-sys = "0.3.70"
 log = "0.4.27"
+send_wrapper = { version = "0.6.0", features = ["futures"] }

crates/iguazu-viewer/src/main.rs
@@ -1,10 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::{egui, CreationContext};
-use egui::{util::History, Direction, Frame, Layout, Rect, UiBuilder};
+use std::sync::Arc;
 
-use iguazu::{schema::{attribute::DefaultView, Entity, EntityStream}, stream::ArcStream};
-use iguazu_egui::{table::TableView, timeline::TimelineView, ViewerContext};
+use async_executor::Executor;
+use eframe::{egui, CreationContext};
+use egui::{util::History, Direction, Frame, Layout, Rect, Ui, UiBuilder};
+
+use iguazu_egui::ViewerContext;
+
+
+mod welcome;
+mod viewer;
+
+use crate::viewer::Viewer;
+use crate::welcome::Welcome;
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> Result<(), eframe::Error> {
@@ -22,7 +31,7 @@ fn main() -> Result<(), eframe::Error> {
     #[command(author, version, about, long_about = None)]
     struct Cli {
         #[clap(flatten)]
-        import: ImportOpts,
+        import: Option<ImportOpts>,
     }
 
     // Log to stdout (if you run with `RUST_LOG=debug`).
@@ -38,13 +47,18 @@ fn main() -> Result<(), eframe::Error> {
         }
     });
 
-    let (mut entity, completion) = block_on(cli.import.import(IMPORTERS, executor.clone())).expect("Failed to load file");
-    block_on(completion).expect("Failed to complete import");
     let storage = MemoryStorage;
 
-    entity.build_summaries(&executor, &storage);
+    let state = if let Some(import) = cli.import {
+        let (mut entity, completion) = block_on(import.import(IMPORTERS, executor.clone())).expect("Failed to load file");
+        block_on(completion).expect("Failed to complete import");
 
-    let view = entity.display_default();
+        entity.build_summaries(&executor, &storage);
+
+        AppState::Viewer(Viewer::new(entity))
+    } else {
+        AppState::Welcome(Welcome::new())
+    };
 
     let options = eframe::NativeOptions {
         ..Default::default()
@@ -53,7 +67,7 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "Iguazu Viewer",
         options,
-        Box::new(|cc| Ok(Box::new(App::new(cc, entity, view)))),
+        Box::new(|cc| Ok(Box::new(App::new(cc, executor, state)))),
     )
 }
 
@@ -67,9 +81,15 @@ fn main() {
 
     let web_options = eframe::WebOptions::default();
 
-    wasm_bindgen_futures::spawn_local(async {
-        use iguazu::{schema::{AttributeMap, Entity, Field, FieldKind}, storage::MemoryStream};
+    let executor = Arc::new(Executor::new());
+    wasm_bindgen_futures::spawn_local({
+        let executor = executor.clone();
+        async move {
+            executor.run(futures_lite::future::pending::<()>()).await;
+        }
+    });
 
+    wasm_bindgen_futures::spawn_local(async {
         let document = web_sys::window()
             .expect("No window")
             .document()
@@ -81,40 +101,13 @@ fn main() {
             .dyn_into::<web_sys::HtmlCanvasElement>()
             .expect("the_canvas_id was not a HtmlCanvasElement");
 
-        let analog = Entity::Data {
-            field: Field::new(FieldKind::Float32),
-            data: MemoryStream::new(&(0..1000).map(|i| (i as f32 * 0.01).sin()).collect::<Vec<f32>>()) as ArcStream,
-            summaries: Default::default(),
-        }.with_attribute("sample_rate", 100.0)
-        .with_attribute("number:range", AttributeMap::from_iter([
-            ("min".into(), (-1.0).into()),
-            ("max".into(), 1.0.into()),
-        ]));
-
-        let digital = Entity::Data{
-            field: Field::new(FieldKind::BitStruct {
-                children: FromIterator::from_iter([
-                    ("bit0".into(), Field::new(FieldKind::Bits { bits: 1 })),
-                    ("bit1".into(), Field::new(FieldKind::Bits { bits: 1 })),
-                    ("bit2".into(), Field::new(FieldKind::Bits { bits: 1 })),
-                    ("bit3".into(), Field::new(FieldKind::Bits { bits: 1 })),
-                ])
-            }),
-            data: MemoryStream::new(&(0..255u8).collect::<Vec<u8>>()) as ArcStream,
-            summaries: Default::default(),
-        }.with_attribute("sample_rate", 32.0);
-
-        let entity = Entity::record()
-            .with_child("analog".into(), analog)
-            .with_child("digital".into(), digital);
-        let view = Some(DefaultView::Timeline);
-        log::debug!("Entity: {entity:?}, view: {view:?}");
+        let state = AppState::Picker { picker_task: None, error: None };
 
         let start_result = eframe::WebRunner::new()
             .start(
                 canvas,
                 web_options,
-                Box::new(move |cc| Ok(Box::new(App::new(cc, entity, view)))),
+                Box::new(move |cc| Ok(Box::new(App::new(cc, executor, state)))),
             )
             .await;
 
@@ -135,15 +128,19 @@ fn main() {
     });
 }
 
-struct App {
-    entity: EntityStream,
-    view: Option<DefaultView>,
-    vctx: ViewerContext,
+enum AppState {
+    Welcome(welcome::Welcome),
+    Viewer(viewer::Viewer),
+}
 
+struct App {
+    vctx: ViewerContext,
+    state: AppState,
     frame_time_history: History<f32>,
 }
+
 impl App {
-    fn new(cc: &CreationContext, entity: Entity<ArcStream>, view: Option<DefaultView>) -> Self {
+    fn new(cc: &CreationContext, executor: Arc<Executor<'static>>, state: AppState) -> Self {
         cc.egui_ctx.set_theme(egui::Theme::Dark);
         cc.egui_ctx.tessellation_options_mut(|o| {
             // Rounding causes jitter and gaps in timeline logic traces
@@ -155,9 +152,8 @@ impl App {
         let max_len = (max_age * 300.0).round() as usize;
 
         App {
-            view,
-            entity,
-            vctx: ViewerContext::new(&cc.egui_ctx),
+            state,
+            vctx: ViewerContext::new(executor, &cc.egui_ctx),
             frame_time_history: History::new(0..max_len, max_age),
         }
     }
@@ -173,27 +169,20 @@ impl eframe::App for App {
 
         let central_panel = Frame::central_panel(&*ctx.style()).inner_margin(0.0);
         egui::CentralPanel::default().frame(central_panel).show(ctx, |ui| {
-            match self.view {
-                Some(DefaultView::Table) => TableView::new().show(&mut self.vctx, ui, &mut self.entity),
-                Some(DefaultView::Timeline) => TimelineView::new().show(&mut self.vctx, ui, &mut self.entity),
-                None => {
-                    ui.label("unknown view");
+            match &mut self.state {
+                AppState::Welcome(welcome) => {
+                    let res = welcome.show(&mut self.vctx, ui);
+                    if let Some(entity) = res.loaded_entity {
+                        self.state = AppState::Viewer(Viewer::new(entity));
+                    }
+                }
+                AppState::Viewer(viewer) => {
+                    viewer.show(&mut self.vctx, ui);
                 }
             }
-
-            let debug = Rect::from_x_y_ranges(ui.max_rect().x_range(), (ui.max_rect().bottom() - 20.0)..=ui.max_rect().bottom());
-            ui.scope_builder(
-                UiBuilder::new()
-                    .max_rect(debug)
-                    .layout(Layout::centered_and_justified(Direction::TopDown)),
-                |ui| {
-                    ui.horizontal(|ui| {
-                        ui.label("Debug:");
-                        self.frame_time_ui(ui);
-                    });
-                }
-            );
         });
+
+        self.debug_ui(ctx);
 
         self.vctx.end();
         self.update_frame_time(ctx, frame);
@@ -201,6 +190,22 @@ impl eframe::App for App {
 }
 
 impl App {
+    fn debug_ui(&mut self, ctx: &egui::Context) {
+        let mut ui = Ui::new(ctx.clone(), egui::Id::new("debug"), UiBuilder::default());
+        let debug = Rect::from_x_y_ranges(ui.max_rect().x_range(), (ui.max_rect().bottom() - 20.0)..=ui.max_rect().bottom());
+        ui.scope_builder(
+            UiBuilder::new()
+                .max_rect(debug)
+                .layout(Layout::centered_and_justified(Direction::TopDown)),
+            |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Debug:");
+                    self.frame_time_ui(ui);
+                });
+            }
+        );
+    }
+
     fn update_frame_time(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         let now = ctx.input(|i| i.time);
         let previous_frame_time = frame.info().cpu_usage;

crates/iguazu-viewer/src/viewer.rs
@@ -1,0 +1,25 @@
+use egui::Ui;
+use iguazu::schema::{attribute::DefaultView, EntityStream};
+use iguazu_egui::{table::TableView, TimelineView, ViewerContext};
+
+pub struct Viewer {
+    entity: EntityStream,
+    view: Option<DefaultView>,
+}
+
+impl Viewer {
+    pub fn new(entity: EntityStream) -> Self {
+        let view = entity.display_default();
+        Self { entity, view }
+    }
+
+    pub fn show(&mut self, vctx: &mut ViewerContext, ui: &mut Ui) {
+        match self.view {
+            Some(DefaultView::Table) => TableView::new().show(vctx, ui, &mut self.entity),
+            Some(DefaultView::Timeline) => TimelineView::new().show(vctx, ui, &mut self.entity),
+            None => {
+                ui.label("unknown view");
+            }
+        }
+    }
+}

crates/iguazu-viewer/src/welcome.rs
@@ -1,0 +1,157 @@
+use std::{sync::Arc, task::Poll};
+
+use async_executor::{Executor, Task};
+use egui::{Button, Color32, Layout, Pos2, Rect, RichText, Ui, UiBuilder, Vec2};
+use iguazu::{import::IMPORTERS, io::ReadableFile, schema::{AttributeMap, Entity, EntityStream, Field, FieldKind}, storage::{MemoryStorage, MemoryStream}, stream::ArcStream};
+use iguazu_egui::ViewerContext;
+#[cfg(not(target_arch = "wasm32"))]
+use rfd::AsyncFileDialog;
+
+pub struct Welcome {
+    picker_task: Option<Task<Option<Result<EntityStream, String>>>>,
+    error: Option<String>,
+}
+
+pub struct WelcomeResponse {
+    pub loaded_entity: Option<EntityStream>,
+}
+
+impl Welcome {
+    pub fn new() -> Self {
+        Self {
+            picker_task: None,
+            error: None,
+        }
+    }
+
+    pub fn show(&mut self, vctx: &mut ViewerContext, ui: &mut Ui) -> WelcomeResponse {
+        centered_box(ui, Vec2::new(300.0, 100.0), Layout::top_down(egui::Align::Center), |ui| {
+            let mut loaded_entity = None;
+
+            ui.style_mut().text_styles.insert(
+                egui::TextStyle::Button,
+                egui::FontId::new(28.0, eframe::epaint::FontFamily::Proportional),
+            );
+            ui.style_mut().spacing.item_spacing.y = 16.0;
+
+            if self.picker_task.is_some() {
+                ui.disable();
+            }
+
+            if ui.add_sized((ui.available_width(), 0.0), Button::new("Open file…")).clicked() {
+                self.error = None;
+                self.picker_task = Some(vctx.spawn(pick_and_import_file(vctx.executor().clone())));
+            }
+
+            if let Some(t) = &mut self.picker_task && let Poll::Ready(res) = vctx.poll_unpin(t) {
+                self.picker_task = None;
+                if let Some(file) = res {
+                    match file {
+                        Ok(entity) => {
+                            loaded_entity = Some(entity);
+                        }
+                        Err(e) => {
+                            self.error = Some(e);
+                        }
+                    }
+                }
+            }
+
+            if ui.add_sized((ui.available_width(), 0.0),Button::new("Load demo data")).clicked() {
+                loaded_entity = Some(generate_demo_entity());
+            }
+
+            if let Some(error) = &self.error {
+                ui.heading(RichText::from("Error").color(Color32::RED));
+                ui.colored_label(Color32::RED, error);
+            }
+
+            WelcomeResponse { loaded_entity }
+        })
+    }
+}
+
+
+fn centered_box<R>(ui: &mut Ui, size: Vec2, layout: Layout, child_ui: impl FnOnce(&mut Ui) -> R) -> R {
+    let available_rect = ui.available_rect_before_wrap();
+    let center = available_rect.center();
+    let top_left = center - size * 0.5;
+    let box_rect = Rect::from_min_size(Pos2::from(top_left), size);
+
+    ui.scope_builder(
+        UiBuilder::new().max_rect(box_rect).layout(layout),
+        child_ui
+    ).inner
+}
+
+async fn pick_file() -> Option<Arc<dyn ReadableFile>> {
+    #[cfg(not(target_arch = "wasm32"))]
+    let res = AsyncFileDialog::new().pick_file().await;
+
+    #[cfg(target_arch = "wasm32")]
+    let res = send_wrapper::SendWrapper::new(AsyncFileDialog::new().pick_file()).await.map(|f| send_wrapper::SendWrapper::new(f));
+
+    if let Some(r) = res {
+        #[cfg(not(target_arch = "wasm32"))]
+        let file = iguazu::io::FsFile::open(r.inner().to_owned()).await.ok()?;
+
+        #[cfg(target_arch = "wasm32")]
+        let file = iguazu::io::WebFile::new(r.inner().clone());
+        Some(Arc::new(file) as Arc<dyn ReadableFile>)
+    } else {
+        None
+    }
+}
+
+async fn pick_and_import_file(executor: Arc<Executor<'static>>) -> Option<Result<EntityStream, String>> {
+    let file = pick_file().await?;
+    let filename = file.filename().unwrap_or("").to_owned();
+
+    let Some(format) = IMPORTERS.first_for_filename(&filename) else {
+        return Some(Err(format!("No import format matched filename `{}`", filename)))
+    };
+
+    let importer = format.import(file);
+    let (mut entity, _completion) = match importer.import(None, executor.clone()).await {
+        Ok(v) => v,
+        Err(e) => { return Some(Err(format!("Failed to import {}: {}", filename, e))); }
+    };
+
+    let storage = MemoryStorage;
+    entity.build_summaries(&executor, &storage);
+
+    Some(Ok(entity))
+}
+
+fn generate_demo_entity() -> EntityStream {
+    let analog = Entity::Data {
+        field: Field::new(FieldKind::Float32),
+        data: MemoryStream::new(&(0..1000).map(|i| (i as f32 * 0.01).sin()).collect::<Vec<f32>>()) as ArcStream,
+        summaries: Default::default(),
+    }.with_attribute("sample_rate", 100.0)
+    .with_attribute("number:range", AttributeMap::from_iter([
+        ("min".into(), (-1.0).into()),
+        ("max".into(), 1.0.into()),
+    ]))
+    .with_attribute("display:accent_color", "blue");
+
+    let digital = Entity::Data{
+        field: Field::new(FieldKind::BitStruct {
+            children: FromIterator::from_iter([
+                ("bit0".into(), Field::new(FieldKind::Bits { bits: 1 }).with_attribute("display:accent_color", "red")),
+                ("bit1".into(), Field::new(FieldKind::Bits { bits: 1 }).with_attribute("display:accent_color", "orange")),
+                ("bit2".into(), Field::new(FieldKind::Bits { bits: 1 }).with_attribute("display:accent_color", "yellow")),
+                ("bit3".into(), Field::new(FieldKind::Bits { bits: 1 }).with_attribute("display:accent_color", "green")),
+            ])
+        }),
+        data: MemoryStream::new(&(0..255u8).collect::<Vec<u8>>()) as ArcStream,
+        summaries: Default::default(),
+    }.with_attribute("sample_rate", 25.0);
+
+    Entity::record()
+        .with_child("analog".into(), analog)
+        .with_child("digital".into(), digital)
+        .with_attribute("display:default", AttributeMap::from_iter([
+            ("view".into(), "timeline".into()),
+        ]))
+}

commit 94262fd2b5f5a274192867b162cd40e02014498e

Workaround for clap panic on `iguazu convert`
```
Found non-required positional argument with a lower index than a required positional argument: "filename" index Some(1)
```

The ImportOptions was made non-required with a requred field so that the group could be optional, and thus ExportOptions must have the same treatment.

Fixes: d1465ce1e10d119097e01d26b94597e74f6cac70

crates/iguazu/cli.rs
@@ -104,7 +104,9 @@ impl ImportOpts {
 }
 
 #[derive(Args, Clone, Debug)]
+#[group(requires = "out_filename")]
 pub struct ExportOpts {
+    #[arg(required = false)]
     pub out_filename: PathBuf,
 
     #[clap(short = 'F', long)]