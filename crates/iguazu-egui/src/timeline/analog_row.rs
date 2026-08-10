4400c1def10f5cce4869dad49c8d6eb5660ff709
Kevin Mehall <km@kevinmehall.net>
2026-08-10 17:41:12 -0600

    timeline: analog: Support nonuniform timebase

--- crates/iguazu-egui/src/timeline/analog_row.rs
+++ crates/iguazu-egui/src/timeline/analog_row.rs
@@ -1,10 +1,10 @@
 use ecow::EcoString;
 use egui::{emath::GuiRounding, Pos2, Rangef, Rect, Stroke, Vec2};
-use iguazu::{IdxRange, schema::{FieldRef, attribute::display::AccentColor}, time::{Time, TimeRange}, view::{RangeView, Timebase}};
+use iguazu::{IdxRange, schema::{FieldRef, attribute::display::AccentColor}, time::{Time, TimeRange}, view::{RangeView, Span, Timebase}};
 
 use crate::{color::named_color, ViewerContext};
 
-use super::{label_frame, stream_rect, TimelineResponse};
+use super::{TimelineResponse, label_frame, scale::Scale, stream_rect};
 
 struct YScale {
     scale: f64,
@@ -27,8 +27,10 @@ impl YScale {
     }
 }
 
+const STROKE_WIDTH: f32 = 1.0;
+
 pub(crate) struct YAxisRow<'a> {
-    sample_rate: f64,
+    timebase: Timebase<'a>,
     y_range: (f64, f64),
     view: RangeView<'a>,
     color: AccentColor,
@@ -36,8 +38,7 @@ pub(crate) struct YAxisRow<'a> {
 }
 
 impl<'a> YAxisRow<'a> {
-    pub fn field(vcx: &'a ViewerContext, field: FieldRef<'_>, timebase: &Timebase<'_>, color: Option<AccentColor>, label: EcoString) -> Option<YAxisRow<'a>> {
-        let sample_rate = timebase.uniform_sample_rate()?;
+    pub fn field(vcx: &'a ViewerContext, field: FieldRef<'_>, timebase: &Timebase<'a>, color: Option<AccentColor>, label: EcoString) -> Option<YAxisRow<'a>> {
         let view = RangeView::new(&vcx.view_manager, field)?;
         let color = color.unwrap_or(AccentColor::Green);
         let y_range = field.field.number_range().or(view.bounds())?;
@@ -47,7 +48,7 @@ impl<'a> YAxisRow<'a> {
         }
 
         Some(YAxisRow {
-            sample_rate,
+            timebase: timebase.clone(),
             y_range,
             view,
             color,
@@ -56,49 +57,75 @@ impl<'a> YAxisRow<'a> {
     }
 
     pub fn time_range(&self) -> Option<TimeRange> {
-        Some(TimeRange {
-            min: Time::ZERO,
-            max: (self.view.state().end as i128) * Time::period_float(self.sample_rate),
-        })
+        match &self.timebase {
+            Timebase::Fixed(sample_rate) => Some(TimeRange {
+                min: Time::ZERO,
+                max: (self.view.state().end as i128) * Time::period_float(*sample_rate),
+            }),
+            Timebase::Nonuniform(v) => {
+                v.time_range()
+            }
+        }
     }
 
-    pub fn render(&self, ui: &mut egui::Ui, scale: &super::scale::Scale) -> TimelineResponse{
+    pub fn render(&self, ui: &mut egui::Ui, scale: &Scale) -> TimelineResponse{
         label_frame(ui, |ui| {
             ui.label(self.label.as_str());
         });
         let rect = stream_rect(ui, scale);
 
-        let stroke_width = 1.0;
-
-        let padded_rect = rect.shrink2(Vec2::new(0.0, 8.0 + stroke_width * 2.0));
+        let padded_rect = rect.shrink2(Vec2::new(0.0, 8.0 + STROKE_WIDTH * 2.0));
 
         if !ui.is_rect_visible(padded_rect) {
             return TimelineResponse::default();
         }
 
-        let idx_scale = scale.idx_scale(self.sample_rate);
-
         let color = named_color(self.color);
 
         let painter = ui.painter_at(rect);
-        let stroke = Stroke::new(stroke_width, color);
 
+        let y_scale = YScale::new(self.y_range, padded_rect.y_range());
+        draw_y_ticks(ui, self.y_range, &y_scale, rect);
+
+        match self.timebase {
+            Timebase::Fixed(sample_rate) => {
+                self.render_fixed_rate(ui, painter, scale, sample_rate, y_scale, color);
+            }
+            Timebase::Nonuniform(ref time_view) => {
+                self.render_nonuniform(ui, painter, scale, time_view, y_scale, color);
+            }
+        }
+
+
+
+        TimelineResponse {
+            snap_to_time: None,
+        }
+    }
+
+    fn render_fixed_rate(&self,
+        ui: &mut egui::Ui,
+        painter: egui::Painter,
+        scale: &Scale,
+        sample_rate: f64,
+        y_scale: YScale,
+        color: egui::Color32,
+    ) {
+        let idx_scale = scale.idx_scale(sample_rate);
         let state = self.view.state();
         let x_range = IdxRange {
             min: idx_scale.visible.min,
             max: (idx_scale.visible.max + 1).min(state.end),
         };
 
-        let y_scale = YScale::new(self.y_range, padded_rect.y_range());
-        draw_y_ticks(ui, self.y_range, &y_scale, rect);
+        let level = idx_scale.min_visible_width_log2(ui.pixels_per_point());
+        let stroke = Stroke::new(STROKE_WIDTH, color);
 
         let mut last: Option<Pos2> = None;
 
-        let dot_opacity = ((idx_scale.points_per_index() - 4.0 * stroke_width) / 8.0).clamp(0.0, 1.0);
-        let dot_color = color.gamma_multiply(dot_opacity);
-        let level = idx_scale.min_visible_width_log2(ui.pixels_per_point());
-
         if level == 0 {
+            let (dot_opacity, dot_color) = get_dot_opacity(color, idx_scale.points_per_index());
+
             for (idx, val) in x_range.into_iter().zip(self.view.iter_base(x_range)) {
                 let Some(val) = val else {
                     last = None;
@@ -111,7 +138,7 @@ impl<'a> YAxisRow<'a> {
                 };
 
                 if dot_opacity > 0.0 {
-                    painter.circle_filled(pos, stroke_width * 2.0, dot_color);
+                    painter.circle_filled(pos, STROKE_WIDTH * 2.0, dot_color);
                 }
 
                 if let Some(lpos) = last {
@@ -145,11 +172,105 @@ impl<'a> YAxisRow<'a> {
                 painter.line_segment([pos1, pos2], stroke);
             }
         }
+    }
 
-        TimelineResponse {
-            snap_to_time: None,
+    fn render_nonuniform(
+        &self,
+        ui: &mut egui::Ui,
+        painter: egui::Painter,
+        scale: &Scale,
+        time_view: &iguazu::view::TimestampView<'_>,
+        y_scale: YScale,
+        color: egui::Color32
+    ) {
+        let idx_scale = scale.idx_scale(time_view.time_rate());
+        let visible_t_range = IdxRange {
+            min: idx_scale.visible.min,
+            max: idx_scale.visible.max + 1,
+        };
+
+        let stroke = Stroke::new(STROKE_WIDTH, color);
+        let min_visible = idx_scale.min_visible_width(ui.pixels_per_point());
+
+        let mut last: Option<Pos2> = None;
+        let mut last_dx = f32::INFINITY;
+
+        for span in time_view.iter(visible_t_range.into(), min_visible.get()) {
+            match span {
+                Span::Loading => {
+                    last = None;
+                    last_dx = f32::INFINITY;
+                }
+                Span::Sparse(idx_range, t_range) | Span::Dense(0, idx_range, t_range) => {
+                    let (Some(val1), Some(val2)) = (self.view.get_base(idx_range.min), self.view.get_base(idx_range.max)) else {
+                        last = None;
+                        last_dx = f32::INFINITY;
+                        continue;
+                    };
+
+                    let pos1 = Pos2 {
+                        x: idx_scale.x_from_idx(t_range.start),
+                        y: y_scale.y_from_value(val1),
+                    };
+                    let pos2 = Pos2 {
+                        x: idx_scale.x_from_idx(t_range.end),
+                        y: y_scale.y_from_value(val2),
+                    };
+
+                    painter.line_segment([pos1, pos2], stroke);
+
+                    let dx = pos2.x - pos1.x;
+                    let (dot_opacity, dot_color) = get_dot_opacity(color, dx.min(last_dx));
+
+                    if dot_opacity > 0.0 {
+                        painter.circle_filled(pos1, STROKE_WIDTH * 2.0, dot_color);
+                    }
+
+                    last_dx = dx;
+                    last = Some(pos2);
+                }
+                Span::Dense(level, idx_range, t_range) => {
+                    let Some((min, max)) = self.view.get_at_level(level, idx_range.min) else {
+                        last = None;
+                        last_dx = f32::INFINITY;
+                        continue;
+                    };
+
+                    let pos1 = Pos2 {
+                        x: idx_scale.x_from_idx(t_range.start),
+                        y: y_scale.y_from_value(min),
+                    };
+                    let pos2 = Pos2 {
+                        x: idx_scale.x_from_idx(t_range.end),
+                        y: y_scale.y_from_value(max),
+                    };
+
+                    if let Some(lpos) = last {
+                        painter.line_segment([lpos, pos1], stroke);
+                    }
+
+                    last = Some(pos2);
+                    last_dx = 0.0;
+
+                    painter.line_segment([pos1, pos2], stroke);
+                }
+            }
+        }
+
+        if let Some(lpos) = last {
+            let (dot_opacity, dot_color) = get_dot_opacity(color, last_dx);
+
+            if dot_opacity > 0.0 {
+                painter.circle_filled(lpos, STROKE_WIDTH * 2.0, dot_color);
+            }
         }
     }
+}
+
+fn get_dot_opacity(color: egui::Color32, dx: f32) -> (f32, egui::Color32) {
+    let dot_opacity = ((dx - 4.0 * STROKE_WIDTH) / 8.0).clamp(0.0, 1.0);
+    let dot_color = color.gamma_multiply(dot_opacity);
+    (dot_opacity, dot_color)
 }
 
 fn generate_ticks(v_range: (f64, f64), desired_tick_count: f64) -> impl Iterator<Item = f64> {
